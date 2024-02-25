fb0d7eedbe488f2ad11a02b153c63350810bd5f8
dabinlyu0406 <151616435+dabinlyu0406@users.noreply.github.com>
2024-02-25 19:27:46 +0900

    config 변경

--- my-flask/config/production.py
+++ my-flask/config/production.py
@@ -1,7 +1,5 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
+SECRET_KEY = b'\x19U\xe0(.\x98\x19!\x18#aH\xc9\x82r\xe9'
